f5c672e8f3ee75879a33464ea4d8bee224a3c7ae
DanniHills <danixahills@gmail.com>
2021-02-20 23:21:28 -0300

    update servicio obtener puesto con producto

--- feria3d/src/services/PuestosService.js
+++ feria3d/src/services/PuestosService.js
@@ -3,7 +3,18 @@ export default class PuestosService {
     static rute = 'http://localhost:8080/api/PuestoArtesanal';
 
     static async obtenerPuestos() {
-        const res = await fetch(`${PuestosService.rute}/all/`);
+        const res = await fetch(`${PuestosService.rute}/getProductos/`);
+        if (res.ok) {
+            const jsonData = await res.json();
+            return jsonData;
+        } else {
+            console.log(res);
+            throw new Error(res.status);
+        }
+    }
+
+    static async obtenerPuestoConProductos(pArt_id) {
+        const res = await fetch(`${PuestosService.rute}/getProductos/` + pArt_id);
         if (res.ok) {
             const jsonData = await res.json();
             return jsonData;
